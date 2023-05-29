5c9f9ddef1ea51514cd13eda47b9ef6910d85bd3
syco <syco@localhost.localdomain>
2023-05-29 15:21:41 -0400

    material ui initialized

---
+++ frontend/app/app/in/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Drawer from '../../components/Drawer' 
+function page() {
+    return (
+        <Drawer/>
+    )
+}
+
+export default page
--- frontend/app/app/page.js
+++ frontend/app/app/page.js
@@ -1,7 +1,15 @@
 import styles from './page.module.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className={styles.container}>FreeSpace</div>
+    <div className={styles.container}>
+      <div className={styles.header}>
+
+      <div><Image className={styles.image} src="/chatting.svg" height={900} width={700} alt="logo"/>
+      </div>
+      </div>
+      {/* </div> */}
+      </div>
   )
 }
